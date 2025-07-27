9ae392715f9c0836cf77bd12067b2c97bea39616
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-07-27 15:11:14 +0300

    Add viewmodel with AccuWeather Hellper

---
+++ weather_app_wpf_mvvm/ViewModel/AccuWeatherHelpers/AccuWeatherHelper.cs
@@ -0,0 +1,76 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherApp.Model;
+
+namespace weather_app_wpf_mvvm.ViewModel.AccuWeatherHelpers
+{
+	public class AccuWeatherHelper
+	{
+		private string baseURL = "http://dataservice.accuweather.com";
+		private string appKey = "";
+		private string locationURL = "locations/v1/cities/autocomplete?apikey={0}&q={1}";
+		private string curentConditionURL = "currentconditions/v1/{0}?apikey={1}";
+
+		public AccuWeatherHelper(string appKey)
+		{
+			this.appKey = appKey;
+		}
+
+		public async Task<List<City>> GetCities(string query)
+		{
+			List<City> cities = new List<City>();
+			if (query != null && this.appKey != "" && query != "")
+			{
+				string url = this.baseURL + "/" + string.Format(this.locationURL, this.appKey, query);
+
+				using (HttpClient client = new HttpClient())
+				{
+					var response = await client.GetAsync(url);
+
+					string json = await response.Content.ReadAsStringAsync();
+					if (!string.IsNullOrEmpty(json))
+					{
+						List<City>  deserializedResult = JsonConvert.DeserializeObject<List<City>>(json);
+						if(deserializedResult != null && deserializedResult.Count > 0)
+						{
+							cities = deserializedResult;
+						}
+					}
+					
+				}
+			}
+			return cities;
+		}
+
+		public async Task<WeatherCondition> GetWeatherCondition(string cityKey)
+		{
+			WeatherCondition currentCondition = new WeatherCondition();
+			if (cityKey != null && this.appKey != "" && cityKey != "")
+			{
+				string url = this.baseURL + "/" + string.Format(this.curentConditionURL, cityKey, this.appKey);
+
+				using (HttpClient client = new HttpClient())
+				{
+					var response = await client.GetAsync(url);
+
+					string json = await response.Content.ReadAsStringAsync();
+					if (!string.IsNullOrEmpty(json))
+					{
+						List<WeatherCondition> deserializedResult = JsonConvert.DeserializeObject<List<WeatherCondition>>(json);
+						if (deserializedResult != null && deserializedResult.Count > 0)
+						{
+							currentCondition = deserializedResult[0];
+						}
+					}
+
+				}
+			}
+			return currentCondition;
+		}
+	}
+}
