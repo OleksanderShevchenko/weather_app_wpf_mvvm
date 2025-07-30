b19dcfb364f84b311f2c44b8838f7e6569798dd9
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-07-30 17:52:30 +0300

    Add two new ptoperties - WeatherConditions and SelectedCity

--- weather_app_wpf_mvvm/ViewModel/WeatherVM.cs
+++ weather_app_wpf_mvvm/ViewModel/WeatherVM.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WeatherApp.Model;
 
 namespace weather_app_wpf_mvvm.ViewModel
 {
@@ -23,7 +24,31 @@ namespace weather_app_wpf_mvvm.ViewModel
 				}
 			}
 		}
-		
+
+		private WeatherCondition _weatherConditions;
+
+		public WeatherCondition WeatherConditions
+		{
+			get { return _weatherConditions; }
+			set 
+			{ 
+				_weatherConditions = value;
+				OnPropertyChanged(nameof(WeatherConditions));
+			}
+		}
+
+		private City _selectedCity;
+
+		public City SelectedCity
+		{
+			get { return _selectedCity; }
+			set 
+			{ 
+				_selectedCity = value;
+				OnPropertyChanged(nameof(SelectedCity));
+			}
+		}
+
 
 		public event PropertyChangedEventHandler PropertyChanged;
 		protected void OnPropertyChanged(string propertyName)
