cb4142ab84da8343a70a1d68e8fb4fe616f0ce93
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-07-31 21:13:05 +0300

    Implement add remove handlers for CanExecuteChanged event. Implement CanExecute method

--- weather_app_wpf_mvvm/ViewModel/Commands/SearchComand.cs
+++ weather_app_wpf_mvvm/ViewModel/Commands/SearchComand.cs
@@ -11,7 +11,11 @@ namespace weather_app_wpf_mvvm.ViewModel.Commands
 	{
 		public WeatherVM VM { get; set; }
 
-		public event EventHandler CanExecuteChanged;
+		public event EventHandler CanExecuteChanged
+		{
+			add { CommandManager.RequerySuggested += value; }
+			remove { CommandManager.RequerySuggested -= value; }
+		}
 
 		public SearchComand(WeatherVM vm)
 		{
@@ -22,8 +26,14 @@ namespace weather_app_wpf_mvvm.ViewModel.Commands
 
 		public bool CanExecute(object parameter)
 		{
+			string query = parameter as string;
+			if(string.IsNullOrWhiteSpace(query))
+			{
+				return false; // Disable the command if the query is empty
+			}
 			return true;
 		}
+
 		public async void Execute(object parameter)
 		{
 			VM.FetchCitiesAsync();
