f6fa3ff4e3a81e209a50da2d5f90a025599e976f
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-07-31 11:15:07 +0300

    Add SearchCommand for fetching cities with autocomplete

---
+++ weather_app_wpf_mvvm/ViewModel/Commands/SearchComand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace weather_app_wpf_mvvm.ViewModel.Commands
+{
+    public class SearchComand: ICommand
+	{
+		public WeatherVM VM { get; set; }
+
+		public event EventHandler CanExecuteChanged;
+
+		public SearchComand(WeatherVM vm)
+		{
+			this.VM = vm;
+			//// Subscribe to the PropertyChanged event of the VM to update CanExecute state
+			//this.VM.PropertyChanged += (s, e) => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+		}
+
+		public bool CanExecute(object parameter)
+		{
+			return true;
+		}
+		public async void Execute(object parameter)
+		{
+			VM.FetchCitiesAsync();
+		}
+	}
+    
+}
