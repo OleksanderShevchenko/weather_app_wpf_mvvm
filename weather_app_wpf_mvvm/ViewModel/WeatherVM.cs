db9333ba62490b239f55269141a68dd7298469ab
Oleksander  Shevchenko <oleksandr.shevchenko@damen.com>
2025-08-01 11:43:41 +0300

    Correct type of temperature value - shall be string

--- weather_app_wpf_mvvm/ViewModel/WeatherVM.cs
+++ weather_app_wpf_mvvm/ViewModel/WeatherVM.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 using weather_app_wpf_mvvm.Core;
 using weather_app_wpf_mvvm.ViewModel.Commands;
 using WeatherApp.Model;
@@ -13,38 +7,41 @@ using WeatherApp.ViewModel.AccuWeatherHelpers;
 
 namespace weather_app_wpf_mvvm.ViewModel
 {
-    public class WeatherVM: INotifyPropertyChanged
-    {
+	public class WeatherVM : INotifyPropertyChanged
+	{
 		private string _cityQuery;
-		private AppConfig _appConfig = AppConfig.Instance;
 		private AccuWeatherHelper _helper;
+		private WeatherCondition _weatherConditions;
+		private ObservableCollection<City> _cities;
 
 		public string CityQuery
 		{
 			get { return _cityQuery; }
 			set
 			{
-				if (_cityQuery != value)
-				{
-					_cityQuery = value;
-					OnPropertyChanged(nameof(CityQuery));
-				}
+				_cityQuery = value;
+				OnPropertyChanged(nameof(CityQuery));
 			}
 		}
-
-		private WeatherCondition _weatherConditions;
 
 		public WeatherCondition WeatherConditions
 		{
 			get { return _weatherConditions; }
-			set 
-			{ 
+			set
+			{
 				_weatherConditions = value;
 				OnPropertyChanged(nameof(WeatherConditions));
 			}
 		}
 
-		private ObservableCollection<City> _cities;
+		private async void getCurrentCondition()
+		{
+			WeatherConditions = await _helper.GetWeatherCondition(SelectedCity.Key);
+			// clear after we've got new current weather conditions
+			CityQuery = string.Empty;
+			_cities.Clear();
+
+		}
 
 		public ObservableCollection<City> FoundCities
 		{
@@ -58,10 +55,14 @@ namespace weather_app_wpf_mvvm.ViewModel
 		public City SelectedCity
 		{
 			get { return _selectedCity; }
-			set 
-			{ 
+			set
+			{
 				_selectedCity = value;
-				OnPropertyChanged(nameof(SelectedCity));
+				if (_selectedCity != null)
+				{
+					OnPropertyChanged(nameof(SelectedCity));
+					getCurrentCondition();
+				}
 			}
 		}
 
@@ -73,19 +74,18 @@ namespace weather_app_wpf_mvvm.ViewModel
 			set { _searchCity = value; }
 		}
 
-
 		public WeatherVM()
 		{
-			if(DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
+			if (DesignerProperties.GetIsInDesignMode(new System.Windows.DependencyObject()))
 			{
-				// Initialize properties if necessary
+				// Initialize properties for design mode
+				CityQuery = "Search for a city...";
 				WeatherConditions = new WeatherCondition()
 				{
 					WeatherText = "Partially cloudy",
 					Temperature = new Temperature()
 					{
-						Metric = new UnitValue() { Value = 20, Unit = "C", UnitType = 1 },
-						Imperial = new UnitValue() { Value = 68, Unit = "F", UnitType = 2 }
+						Metric = new UnitValue() { Value = "20", Unit = "C", UnitType = 1 },
 					},
 					Link = "http://www.accuweather.com/en/ua/uzhhorod/326310/current-weather/326310?lang=en-us",
 				};
@@ -93,33 +93,45 @@ namespace weather_app_wpf_mvvm.ViewModel
 				{
 					LocalizedName = "Uzhgorod"
 				};
+				FoundCities = new ObservableCollection<City>
+				{
+					SelectedCity,
+					new City { LocalizedName = "Mykolaiv" }
+				};
 			}
-			// Initialize the AccuWeatherHelper with the app configuration
-			_helper = new AccuWeatherHelper(_appConfig.AppKey, _appConfig.ApiBaseUrl, _appConfig.LocationUrl, _appConfig.ConditionUrl);
-			_searchCity = new SearchComand(this);
-			FoundCities = new ObservableCollection<City>();
+			else
+			{
+				CityQuery = "";
+				// Initialize the AccuWeatherHelper with the app configuration
+				var _appConfig = AppConfig.Instance;
+				FoundCities = new ObservableCollection<City>();
+				_helper = new AccuWeatherHelper(_appConfig.AppKey, _appConfig.ApiBaseUrl, _appConfig.LocationUrl, _appConfig.ConditionUrl);
+				SearchForCity = new SearchComand(this);
+			}
+
 		}
 
-		public async Task FetchCitiesAsync()
+		public async void FetchCitiesAsync()
 		{
 			// This method can be used to make a query to the weather service.
 			// It can be implemented to fetch list of cities using user input in the text box.
 			var cities = await _helper.GetCities(CityQuery);
 			FoundCities.Clear();
-			foreach (var city in cities) 
+			foreach (var city in cities)
 			{
-				FoundCities.Add(city);
+				_cities.Add(city);
 			}
 		}
 
-
 		public event PropertyChangedEventHandler PropertyChanged;
-	
+
 		protected void OnPropertyChanged(string propertyName)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 
-		// Additional properties and methods for weather data can be added here.
+
+
+
 	}
 }
