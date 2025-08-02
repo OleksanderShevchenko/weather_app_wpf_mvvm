8523af755a12454b702d9c13cac7aefaed879d69
Олександр Шевченко <oleksander.shevchenko777@gmail.com>
2025-08-02 10:07:57 +0300

    Add BoolToRain convertor class. Add text block to display is it raining or not. Use convertor class. Remove underline from text with link - only link will has it

---
+++ weather_app_wpf_mvvm/ViewModel/ValueConverters/BoolToRainConvertor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace weather_app_wpf_mvvm.ViewModel.ValueConverters
+{
+    public class BoolToRainConvertor : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			bool rainStatus = (bool)value;
+			if (rainStatus)
+				return "It is raining";
+			return "It is not raining";
+		}
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			string rainStatus = (string)value;
+			if (rainStatus == "It is raining")
+				return true;
+			return false;
+		}
+		
+	}
+   
+}
